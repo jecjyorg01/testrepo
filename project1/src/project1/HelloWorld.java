6ce31fe660121bc4d87768fd47c3afe4d6841646
sugarara <sugarara@jec.ac.jp>
2020-11-10 16:04:33 +0900

    hogehoge

--- project1/src/project1/HelloWorld.java
+++ project1/src/project1/HelloWorld.java
@@ -3,11 +3,8 @@ package project1;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("HelloWorld");
-		System.out.println("Java");
-		System.out.println("HTML5");
-
-		for(int i=0; i<3; i++) System.out.println("Hardware");
+		System.out.println("HelloWorldf");
 	}
 
+	//comment
 }
